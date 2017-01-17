35d37e041ef999f019e75274bfca9408c0984500
David MacKintosh <dgawmackintosh@gmail.com>
2017-01-17 15:57:21 +0000

    Completes routes

--- app.js
+++ app.js
@@ -3,13 +3,30 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes.js');
-
-
 var jsonParser = require('body-parser').json;
+var logger = require('morgan');
 
+app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use("/questions", routes);
+
+//Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error("Not found");
+	error.status = 404;
+	next(err);
+});
+
+//Error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	});
+});
 
 var port = process.env.PORT || 3000;
 
--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ router.get("/", function(req, res) {
 });
 
 //POST /questions
-//Create questions
+//Create a question
 router.post("/", function(req, res) {
 	res.json({
 		response: "You sent me a POST request",
@@ -22,10 +22,61 @@ router.post("/", function(req, res) {
 
 //GET /questions/:id
 //Return specific questions
-router.get("/:id", function(req, res) {
+router.get("/qId", function(req, res) {
 	res.json({
-		response: "You sent me a GET request for ID " + req.params.id
+		response: "You sent me a GET request for ID " + req.params.qId
 	});
+});
+
+//POST /questions/:id/answers
+//Create an answer
+router.post("/:qId/answers", function(req, res) {
+	res.json({
+		response: "You sent me a POST request to /answers",
+		questionId: req.params.qId,
+		body: req.body
+	});
+});
+
+//PUT /questions/:qId/answers/:aId
+//Edit specific answer
+router.put("/:qId/answers/:aId", function(req, res) {
+	res.json({
+		response: "You sent me a PUT request to /answers",
+		questionId: req.params.qId,
+		answerId: req.params.aId,
+		body: req.body
+	})
+});
+
+//DELETE /questions/:qId/answers/:aId
+//Delete specific answer
+router.delete("/:qId/answers/:aId", function(req, res) {
+	res.json({
+		response: "You sent me a DELETE request to /answers",
+		questionId: req.params.qId,
+		answerId: req.params.aId
+	})
+});
+
+//POST /questions/:qId/answers/:aId/vote-up
+//POST /questions/:qId/answers/:aId/vote-down
+//Vote on specific answer
+router.post("/:qId/answers/:aId/vote-:dir", function(req, res, next) {
+		if(req.params.dir.search(/^(up|down)$/) === -1) {
+			var err = new Error("Not found");
+			err.status = 404;
+			next(err);
+		} else {
+			next();
+		}
+	}, function(req, res) {
+		res.json({
+			response: "You sent me a POST request to /vote-" + req.params.dir,
+			questionId: req.params.qId,
+			answerId: req.params.aId,
+			vote: req.params.dir
+		})
 });
 
 module.exports = router;
